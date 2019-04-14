3cf3eb983acd5e079d27e27f1ddcb1d8a8c8821b
sverona <sasham@fastmail.com>
2019-04-14 17:40:02 -0500

    scraping continues

--- batch-compile.sh
+++ batch-compile.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-CHANNELS=channels
-CACHE=cache
-VIDEOS=videos.tsv
-
+CHANNELS=${1:-channels}
+CACHE=${2:-cache}
+VIDEOS=${3:-videos.tsv}
+SEP='<<>>'
 
 touch $CHANNELS $CACHE $VIDEOS
 
 for channel in $(cat $CHANNELS); do
     echo $channel
     if ! grep -q $channel $CACHE; then
-        youtube-dl --get-filename  -o "%(id)s<<>>%(title)s<<>>%(channel_id)s<<>>%(uploader)s"  "https://youtube.com/channel/$channel" | sed -u -e "s/<<>>/\t/g" | tee -a videos.tsv
+        youtube-dl --get-filename  -o "%(id)s$SEP%(title)s$SEP%(channel_id)s$SEP%(uploader)s"  "https://youtube.com/channel/$channel" | sed -u -e "s/$SEP/\t/g" | tee -a videos.tsv
         echo $channel >> $CACHE
     fi
 done
