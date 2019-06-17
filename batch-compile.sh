8aa157cdd112ad052cab248bc00ae11e6b8566c7
sverona <sasham@fastmail.com>
2019-06-17 14:31:52 -0500

    some readability edits

--- batch-compile.sh
+++ batch-compile.sh
@@ -8,16 +8,21 @@ SEP='<<>>'
 red="$(tput setaf 1)"
 reset="$(tput sgr0)"
 
-touch $CHANNELS $CACHE $VIDEOS
+touch "$CHANNELS" "$CACHE" "$VIDEOS"
 
-for channel in $(cat $CHANNELS); do
-
-  if grep -q $channel $CACHE; then
-    count="$(printf '%5d' $(grep -c $channel $VIDEOS))"
+for channel in $(cat "$CHANNELS"); do
+  if grep -q $channel "$CACHE"; then
+    count="$(grep -c $channel $VIDEOS)"
     echo -e "$red($(date '+%x %X'))$reset\tParsed $count videos from $channel"
   else
-    echo -e "$red[$(date '+%x %X')]$reset\tNow parsing $channel"
-    youtube-dl --ignore-errors --get-filename  -o "%(id)s$SEP%(title)s$SEP%(channel_id)s$SEP%(uploader)s"  "https://youtube.com/channel/$channel" | sed -u -e "s/$SEP/\t/g" | tee -a videos.tsv
-    echo $channel >> $CACHE
+    echo -e "$red($(date '+%x %X'))$reset\tNow parsing $channel"
+
+    youtube-dl --ignore-errors --get-filename \
+      -o "%(id)s$SEP%(title)s$SEP%(channel_id)s$SEP%(uploader)s" \
+      "https://youtube.com/channel/$channel" \
+        | sed -u -e "s/$SEP/\t/g" \
+        | tee -a "$VIDEOS"
+
+    echo $channel >> "$CACHE"
   fi
 done
